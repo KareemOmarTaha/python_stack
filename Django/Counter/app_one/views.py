12389327b0c99d2d11c9a99cdc6c93e380814a92
Kareem Taha <kareemtaha2013@gmail.com>
2023-05-29 20:36:27 +0300

    Counter-Django assignment

---
+++ Django/Counter/app_one/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render , redirect
+
+def index(request):
+    if 'counter' in request.session:
+        request.session['counter'] = request.session['counter'] + 1
+
+    else:
+        request.session['counter'] = 1
+
+    context = {
+        'counter': request.session['counter']
+    }
+
+    return render(request, "index.html", context)
+
+
+def destroy (request):
+    del request.session['counter']
+    return redirect('/')
