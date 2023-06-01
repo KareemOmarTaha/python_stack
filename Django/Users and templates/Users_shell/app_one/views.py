24358f62d7c9797556ae431a2a6467249dbc2277
kareem taha <kareemtaha2013@gmail.com>
2023-06-01 16:35:43 +0300

    'Dojo

--- Django/Users and templates/Users_shell/app_one/views.py
+++ Django/Users and templates/Users_shell/app_one/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect
 from .models import users
+
 
 def index(request):
     context = {
-        "all_the_users": users.objects.all()  
+        "all_the_users": users.objects.all()
     }
-    return render (request , "index.html" , context)
+    return render(request, "index.html", context)
+
 
 def add_user(request):
-    request.session['first_name'] = request.POST['first_username'] 
-    request.session['last_name'] = request.POST['last_username'] 
-    request.session['uesr_age'] = request.POST['user_age'] 
-    request.session['user_email'] = request.POST['user_email']
-    new_user = users.objects.create(first_name= request.session['first_name'] , 
-        last_name = request.session['last_name'] , age = request.session['uesr_age'] , 
-        email_address =  request.session['user_email'])
+    first_name = request.POST['first_username']
+    last_name = request.POST['last_username']
+    age = request.POST['user_age']
+    email = request.POST['user_email']
+    new_user = users.objects.create(first_name=first_name,
+                                    last_name=last_name, age=age,
+                                    email_address=email)
     return redirect('/')
